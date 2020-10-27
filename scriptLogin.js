711e7889372fba464e6d74d9d361f32dbb1bec54
Guilherme Bertonzzin <gbertonzzin@gmail.com>
2020-10-27 20:47:30 -0300

    Adicionado aviso senha

--- scriptLogin.js
+++ scriptLogin.js
@@ -1,9 +1,15 @@
-function checkusr() {
+function validar() {
  	var usr = document.forms["entrada"]["usuario"].value;
+	var psw = document.forms["entrada"]["senha"].value;
 	if (usr == "") {
-	document.getElementById("Aviso").style.display = "block"
-	document.getElementById("Aviso").innerHTML = "É necessário inserir seu usuário!";
-	setTimeout(sumirAviso, 3000)
+		document.getElementById("Aviso").style.display = "block"
+		document.getElementById("Aviso").innerHTML = "É necessário inserir seu usuário!";
+		setTimeout(sumirAviso, 3000)
+  	}
+	else if (psw == "") {
+		document.getElementById("Aviso").style.display = "block"
+		document.getElementById("Aviso").innerHTML = "É necessário inserir sua senha!";
+		setTimeout(sumirAviso, 3000)
     	return false;
   	}
 }

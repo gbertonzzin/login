97ac1c80cd6fd1787035dde0e02a710e00c92346
Guilherme Bertonzzin <gbertonzzin@gmail.com>
2020-10-27 20:16:05 -0300

    Alterado aviso

--- scriptLogin.js
+++ scriptLogin.js
@@ -1,7 +1,13 @@
 function checkusr() {
- var usr = document.forms["entrada"]["usuario"].value;
-  if (usr == "") {
-    alert("É necessário inserir seu usuário");
-    return false;
-  }
+ 	var usr = document.forms["entrada"]["usuario"].value;
+	if (usr == "") {
+	document.getElementById("Aviso").style.display = "block"
+	document.getElementById("Aviso").innerHTML = "É necessário inserir seu usuário!";
+	setTimeout(sumirAviso, 3000)
+    	return false;
+  	}
+}
+function sumirAviso () {
+	document.getElementById("Aviso").style.display = "none"
+
 }

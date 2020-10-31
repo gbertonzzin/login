41e8956be1eec48e1e1ef8f21c1bdfd490ddda53
Guilherme Bertonzzin <gbertonzzin@gmail.com>
2020-10-31 11:44:55 -0300

    mudancas no script, pag de recuperacao

--- scriptLogin.js
+++ scriptLogin.js
@@ -1,20 +1,38 @@
+var avisos = ["É necessário inserir seu usuário!", "É necessário inserir sua senha!", "É necessário inserir seu e-mail!"]
+
 function validar() {
- 	var usr = document.forms["entrada"]["usuario"].value;
+	var usr = document.forms["entrada"]["usuario"].value;
 	var psw = document.forms["entrada"]["senha"].value;
-	var avisos = ["É necessário inserir seu usuário!", "É necessário inserir sua senha!"]
 	if (usr == "") {
 		document.getElementById("Aviso").style.display = "block"
 		document.getElementById("Aviso").innerHTML = avisos[0]
 		setTimeout(sumirAviso, 3000)
 		return false;
   	}
-	else if (psw == "") {
+	else if (email == "") {
 		document.getElementById("Aviso").style.display = "block"
-		document.getElementById("Aviso").innerHTML = avisos[1];
+		document.getElementById("Aviso").innerHTML = avisos[2];
 		setTimeout(sumirAviso, 3000)
 		return false;
   	}
 }
+function validarRec() {
+	var usr = document.forms["entrada"]["usuario"].value;
+	var email = document.forms["entrada"]["email"].value;
+	if (usr == "") {
+		document.getElementById("Aviso").style.display = "block"
+		document.getElementById("Aviso").innerHTML = avisos[0]
+		setTimeout(sumirAviso, 3000)
+		return false;
+  	}
+	else if (email == "") {
+		document.getElementById("Aviso").style.display = "block"
+		document.getElementById("Aviso").innerHTML = avisos[2];
+		setTimeout(sumirAviso, 3000)
+		return false;
+  	}
+}
+
 function sumirAviso () {
 	document.getElementById("Aviso").style.display = "none"
 
